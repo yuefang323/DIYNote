3fa8022dba48ebb757ed37066dfa9be3add0332f
Fang Yue <yuefang323@gmail.com>
2022-05-02 14:22:51 -0700

    add notes get route

---
+++ backend/routes/api/notebooks.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const asyncHandler = require("express-async-handler");
+const { Notebook, Note } = require("../../db/models");
+
+const router = express.Router();
+
+
+
+module.exports = router;
--- backend/routes/api/notes.js
+++ backend/routes/api/notes.js
@@ -3,16 +3,24 @@ const asyncHandler = require("express-async-handler");
 const { Note } = require("../../db/models");
 const router = express.Router(); 
 
-// get all notes of a 
+// get all notes of a login user 
 router.get("/:userId", asyncHandler(async(req, res) => {
 
     const userId = req.params.userId;
     const notes = await Note.findAll({
       where: {
-        userId: userId
+        userId
       },
       order: [["updatedAt", "DESC"]],
     })
   
     return res.json(notes)
   }))
+
+// post a new note
+router.post("", asyncHandler(async(req, res) => {
+  
+}))
+
+
+  module.exports = router;
