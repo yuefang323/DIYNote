3df3971eea76162807051ccbc20f444c513d10e6
Fang Yue <yuefang323@gmail.com>
2022-05-02 17:11:37 -0700

    add notebook routes

--- backend/routes/api/notebooks.js
+++ backend/routes/api/notebooks.js
@@ -4,6 +4,33 @@ const { Notebook, Note } = require("../../db/models");
 
 const router = express.Router();
 
+// get a user's all notebooks - see users.js
 
+// create a notebook
+router.post("/", asyncHandler(async(req, res) => {
+    const { name, userId } = req.body; 
+    const newNotebook = await Notebook.create({
+        name, 
+        userId,
+    })
+    return res.json(newNotebook); 
+})); 
+
+// get a notebook detail
+
+router.get("/:id(\\d+)", asyncHandler(async(req, res) => {
+    const notebookId = req.params.id; 
+    const notes = await Note.findAll({
+        where: {
+            notebookId
+        },
+        order: [["updatedAt", "DESC"]],
+    })
+    return res.json(notes); 
+})); 
+
+// update a notebook info
+
+// delete a notebook
 
 module.exports = router;
--- backend/routes/api/notes.js
+++ backend/routes/api/notes.js
@@ -1,26 +1,54 @@
 const express = require("express");
 const asyncHandler = require("express-async-handler");
 const { Note } = require("../../db/models");
-const router = express.Router(); 
+const router = express.Router();
 
-// get all notes of a login user 
-router.get("/:userId", asyncHandler(async(req, res) => {
-
+// get all notes of a login user
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
     const userId = req.params.userId;
     const notes = await Note.findAll({
       where: {
-        userId
+        userId,
       },
       order: [["updatedAt", "DESC"]],
-    })
-  
-    return res.json(notes)
-  }))
+    });
 
+    return res.json(notes);
+  })
+);
 // post a new note
-router.post("", asyncHandler(async(req, res) => {
-  
-}))
+router.post(
+  "/",
+  asyncHandler(async (req, res) => {
+    const { userId, title, content, notebookId } = req.body;
 
+    const newNote = await Note.create({
+      userId,
+      title,
+      content,
+      notebookId,
+    });
 
-  module.exports = router;
+    const notes = await Note.findAll({
+      where: {
+        userId,
+        notebookId,
+      },
+      order: [["updatedAt", "DESC"]],
+    });
+    return res.json(notes);
+  })
+);
+// get a specific note
+router.get(
+  "/:id(\\d+)",
+  asyncHandler(async (req, res) => {
+    const noteId = req.params.id;
+    const note = await Note.findByPk(noteId);
+    return res.json(note);
+  })
+);
+
+module.exports = router;
